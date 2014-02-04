b6cbf7f26897edec9a6701bba819c0a4e8033379
David Lamont <del6597@rit.edu>
2014-02-03 20:15:19 -0500

    Adds School_Module.cs

---
+++ jobSalt/jobSalt/Models/Modules/Jobs/School_Module.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jobSalt.Models.Modules.Jobs
+{
+    public class School_Module : IJobModule
+    {        
+        public School_Module ()
+        {
+            // May need to initialize DisplayName and ResultsPerPage here
+        }
+        public int ResultsPerPage
+        {
+            get
+            {
+                return ResultsPerPage;
+            }
+            set
+            {
+                ResultsPerPage = value;
+            }
+        }
+
+        public string DisplayName
+        {
+            get
+            {
+                return DisplayName;
+            }
+            set
+            {
+                DisplayName = value;
+            }
+        }
+
+        public async List<JobPost> GetJobs(List<Filter> filters, int page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
