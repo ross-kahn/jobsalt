1213b0c7a5ccbd0852d50b3b0460e37c712ba53e
Christopher Ray <cray1@email.arizona.edu>
2014-02-06 16:11:43 -0700

    Added FilterListToUrlQueryString to jobSalt.Models.Filter

--- jobSalt/jobSalt/Models/Filter.cs
+++ jobSalt/jobSalt/Models/Filter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Text;
 namespace jobSalt.Models
 {
     public enum Field { Source, Date, CompanyName, JobTitle, Location, Salary, Description, FieldOfStudy, Keyword};
@@ -17,7 +17,11 @@ namespace jobSalt.Models
             this.TargetField = targetField;
             this.Value = value;
         }
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="filters"></param>
+		/// <returns></returns>
         public static Dictionary<Field, List<string>> FilterListToDictionary(List<Filter> filters)
         {
             // Create a dictionary with Key => FilterType and Value => The actual filter
@@ -41,6 +45,29 @@ namespace jobSalt.Models
 
             return FilterHash;
         }
+
+		/// <summary>
+		/// Creates a partial url query string from an IEnumerable list of filters.
+		/// This query string needs to be appended to the full URL. 
+		/// For example, if "?filterStrings={Source,Indeed}{Keyword,Computers}" is returned,
+		/// then the full URL could be "http://localhost:38087/Job?filterStrings={Source,Indeed}{Keyword,Computers}".
+		/// </summary>
+		/// <param name="filters">The filters to create a query string from.</param>
+		/// <returns>A URL query string corresponding to the given list of filters.</returns>
+		public static String FilterListToUrlQueryString ( IEnumerable<Filter> filters )
+			{
+			//build the queryString
+			StringBuilder queryString = new StringBuilder( );
+
+			//fill with each filter in the form of {filterTarget,value}
+			foreach ( Filter currentFilter in filters.Distinct() )
+				{
+				queryString.Append( "{"+currentFilter.TargetField+","+currentFilter.Value+"}" );
+				}
+
+			//return
+			return queryString as String;
+			}
 
 
     }
