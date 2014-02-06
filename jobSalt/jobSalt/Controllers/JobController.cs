1f4c451e219a52f85452b9fa59569732d664b0eb
Christopher Ray <cray1@email.arizona.edu>
2014-02-06 13:27:51 -0700

    changed to use query string from web api

--- jobSalt/jobSalt/Controllers/JobController.cs
+++ jobSalt/jobSalt/Controllers/JobController.cs
@@ -11,35 +11,40 @@ namespace jobSalt.Controllers
     {
         private JobShepard shepard = new JobShepard();
 
-        public ActionResult Index(List<Models.Filter> filters = null, int page = 1, int resultsPerPage = 10, String filterStrings="")
+        public ActionResult Index(List<Models.Filter> filters = null, int page = 1, int resultsPerPage = 10)
         { 
 			// If filters is null replace it
             filters = filters ?? new List<Models.Filter>();
 
 			//filterStrings is expected to be in the form of "{targetField,value},{targetField1,value1}" without the quotes.
+			String filterStrings= Request.QueryString["filterStrings"];
 
-			//fill filters with filters from filterStrings
-			foreach ( string filterString in filterStrings.Split( new Char[] { '{','}' } ) ) //split into individual filter strings first
+			if ( filterStrings!=null )
 				{
-				//split current filter string into a string array {targetFiled,value}
-				String[] currentFilterString = filterString.Split(new char[]{','});
 
-				if ( currentFilterString.Length==2 && Enum.IsDefined( typeof( Models.Field ) , currentFilterString[0] ) )
+				//fill filters with filters from filterStrings
+				foreach ( string filterString in filterStrings.Split( new Char[] { '{' , '}' } ) ) //split into individual filter strings first
 					{
+					//split current filter string into a string array {targetFiled,value}
+					String[] currentFilterString = filterString.Split( new char[] { ',' } );
 
-					//get target field from Enum
-					Models.Field targetField = (Models.Field)Enum.Parse( typeof( Models.Field ) , currentFilterString[0] );
-
-					//build the current filter
-					Models.Filter filter = new Models.Filter( targetField , currentFilterString[1] );
-
-					//if the filter is not already in the filter list, add it.
-					if ( !filters.Any( a => a.TargetField.Equals( filter.TargetField ) && a.Value.Equals( filter.Value ) ) )
+					if ( currentFilterString.Length==2 && Enum.IsDefined( typeof( Models.Field ) , currentFilterString[0] ) )
 						{
-						filters.Add( filter );
+
+						//get target field from Enum
+						Models.Field targetField = (Models.Field)Enum.Parse( typeof( Models.Field ) , currentFilterString[0] );
+
+						//build the current filter
+						Models.Filter filter = new Models.Filter( targetField , currentFilterString[1] );
+
+						//if the filter is not already in the filter list, add it.
+						if ( !filters.Any( a => a.TargetField.Equals( filter.TargetField ) && a.Value.Equals( filter.Value ) ) )
+							{
+							filters.Add( filter );
+							}
 						}
+
 					}
-				 
 				}
             if (Request.IsAjaxRequest())
             {
