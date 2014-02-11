6d5d1bfb0d674eb6d2df71c242ca3a49573d5870
Daniel Wallach <dywallach@dywallach.net>
2014-02-11 14:50:55 -0500

    Database!

---
+++ jobSalt/jobSalt/Controllers/UserDataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using jobSalt.Models.UserData;
+
+namespace jobSalt.Controllers
+{
+    public class UserDataController : Controller
+    {
+        private UserDataContext database = new UserDataContext("UserDataContextDb");
+        //
+        // GET: /UserData/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add(UserDataRow row)
+        {
+            database.UserDataRows.Add(row);
+            database.SaveChanges();
+            return Content("Data Saved");
+        }
+
+        public ActionResult Delete(string username, string module)
+        {
+            return View();
+        }
+
+        public ActionResult Read(string username, string module)
+        {
+            return View();
+        }
+    }
+}
---
+++ jobSalt/jobSalt/Models/UserData/UserDataRow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jobSalt.Models.UserData
+{
+    public class UserDataRow
+    {
+        public string UserName { get; set; }
+        public string ModuleName { get; set; }
+        public string Data { get; set; }
+    }
+}
