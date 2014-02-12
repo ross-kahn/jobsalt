ff5138451d33b4efb91043951cccdea13cac1333
Colton Presler <C.J.Presler@gmail.com>
2014-02-12 08:34:49 -0500

    Hacked the controller up a little more so that it could get job posts via the LinkedIn API if the API were actually working.

--- jobSalt/jobSalt/Controllers/JobController.cs
+++ jobSalt/jobSalt/Controllers/JobController.cs
@@ -3,8 +3,10 @@ using jobSalt.Models.Modules.Jobs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace jobSalt.Controllers
 {
@@ -14,13 +16,17 @@ namespace jobSalt.Controllers
 
         public ActionResult Index(string filterString, int page = 1, int resultsPerPage = 10)
         {
-            bool debug = true;
-            if (debug)
+            if (Session["LoggedIntoLinkedIn"] == null)
+            {
+                Session["LoggedIntoLinkedIn"] = false;
+            }
+
+            if ((bool)Session["LoggedIntoLinkedIn"] == false)
             {
                 return Redirect("https://www.linkedin.com/uas/oauth2/authorization?response_type=code" +
-                                           "&client_id=75wt0uzfa9hfro" +
+                                           "&client_id=75tu3x63buelpy" +
                                            "&state=DCEEFWF45453sdffef424jobsaltisthebesthingever32472134721324r" +
-                                           "&redirect_uri=http://localhost:38087/");
+                                           "&redirect_uri=http://localhost:38087/Job/LinkedInLogin");
             }
 
 
@@ -36,5 +42,38 @@ namespace jobSalt.Controllers
 
             return View();
         }
+
+        private class linkedInResponse
+        {
+            public string expires_in;
+            public string access_token;
+        }
+
+        public ActionResult LinkedInLogin(string code, string state)
+        {
+            Session["LoggedIntoLinkedIn"] = true;
+
+            using (var client = new WebClient())
+            {
+                string request = "https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code" +
+                                           "&code=" + code +
+                                           "&redirect_uri=http://localhost:38087/Job/LinkedInLogin" +
+                                           "&client_id=75tu3x63buelpy" +
+                                           "&client_secret=hBVkeuOMTd1sCGRe";
+
+                string json = client.DownloadString(request);
+
+                var serializer = new JavaScriptSerializer();
+                linkedInResponse response = serializer.Deserialize<linkedInResponse>(json);
+
+
+                string jobRequest = "https://api.linkedin.com/v1/job-search?keywords=java&sort=DA&oauth2_access_token=" + response.access_token;
+                string jobs = client.DownloadString(jobRequest);
+            }
+
+            return RedirectToAction("Index");
+        }
     }
+
+
 }
