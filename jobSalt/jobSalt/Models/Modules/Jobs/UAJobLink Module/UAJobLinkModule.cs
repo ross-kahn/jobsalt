e6a4816a11470622b9d0eda911b8fb1643415ef3
Christopher Ray <cray1@email.arizona.edu>
2014-02-18 12:55:47 -0700

    Dummy Module for UA Joblink

---
+++ jobSalt/jobSalt/Models/Modules/Jobs/UAJobLink Module/UAJobLinkModule.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jobSalt.Models.Modules.Jobs.UAJobLink_Module
+{
+	public class UAJobLinkModule : IJobModule
+	{
+
+		//Still mostly an empty module for now.
+		// Simplicity data from wildcat joblink needs to be cached first in order to be queried.
+
+		private	Source source = new Source( )
+			{
+				Name = "UAJobLink" ,
+				Icon = @"\Content\images\UA_Block_A.png"
+			};
+		public Source Source
+		{
+			get
+			{
+			return Source;
+			}
+		}
+		/// <summary>
+		/// Asyncronously grap a set of job postings based off of the given filters and page number.
+		/// </summary>
+		/// <param name="filters">The filters that the module should use to query</param>
+		/// <param name="page">What page number of results to grab</param>
+		/// <returns>The job postings</returns>
+		public List<JobPost> GetJobs(List<Filter> filters, int page, int resultsPerPage)
+		{
+		List<JobPost> jobs = new List<JobPost>( );
+		for ( int i = 0 ; i < resultsPerPage ; ++i )
+			{
+			jobs.Add( new JobPost( )
+			{
+				Company = "None" ,
+				DatePosted = DateTime.Now ,
+				Description = "Default Posting from University of Arizona's Wildcat Joblink" ,
+				FieldOfStudy = "Computer Science" ,
+				JobTitle = "Default " + ( ( page-1 )*resultsPerPage+i ) ,
+			} );
+			}
+		return jobs;
+		}
+	}
+}
