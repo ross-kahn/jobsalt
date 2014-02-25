1c33304edc050f7d011567c79aa7ae89ea1873ef
Ross Kahn <ross.kahn@outlook.com>
2014-02-25 13:48:32 -0500

    Getting linkedin temp data to work

--- jobSalt/jobSalt/Models/Modules/Jobs/LinkedIn Module/LinkedInTempData.cs
+++ jobSalt/jobSalt/Models/Modules/Jobs/LinkedIn Module/LinkedInTempData.cs
@@ -14,7 +14,9 @@ namespace jobSalt.Models.Modules.Jobs.LinkedIn_Module
 
         public LinkedInResult getDummyData()
         {
-            string json = File.ReadAllText(@"\Content\example_json_linkedin.txt");
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Content\\example_json_linkedin.txt" ;
+            //bool fileexists = File.Exists(path);
+            string json = File.ReadAllText(path);
             var serializer = new JavaScriptSerializer();
 
             LinkedInResult lResult = serializer.Deserialize<LinkedInResult>(json);
