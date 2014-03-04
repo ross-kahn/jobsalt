0794065b851583747987b517a03dd19fc5c9aa72
David Lamont <del6597@rit.edu>
2014-03-03 22:17:01 -0500

    Add RitAlumniModule

---
+++ jobSalt/jobSalt/Models/Feature/Alumni/School Module/RitAlumniModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jobSalt.Models.Feature.Alumni.School_Module
+{
+    public class RitAlumniModule : IAlumniModule
+    {        
+        Data_Types.Source IAlumniModule.Source
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        List<AlumniPost> IAlumniModule.GetAlumni(Dictionary<Field, string> filters)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<string> IAlumniModule.GetCompanies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
