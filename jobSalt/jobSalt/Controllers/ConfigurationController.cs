dd10ee58e26aa9a31d02e1f09ed99c814aa5e102
Colton Presler <C.J.Presler@gmail.com>
2014-04-09 10:13:31 -0400

    Initial configuration page

---
+++ jobSalt/jobSalt/Controllers/ConfigurationController.cs
@@ -0,0 +1,30 @@
+﻿using jobSalt.Models.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jobSalt.Controllers
+{
+    public class ConfigurationController : Controller
+    {
+        //
+        // GET: /Configuration/
+
+        public ActionResult Index()
+        {
+            return View(ConfigLoader.SiteConfig);
+        }
+        
+        public ActionResult SaveConfig(SiteConfig config)
+        {
+            if(config != null)
+            {
+                ConfigLoader.SiteConfig = config;
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
