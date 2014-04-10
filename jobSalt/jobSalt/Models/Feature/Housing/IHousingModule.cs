c023ee76043affa99e1935d488a4eed129fa5c6d
David Lamont <del6597@rit.edu>
2014-04-10 00:24:14 -0400

    Adds housing interface.

---
+++ jobSalt/jobSalt/Models/Feature/Housing/IHousingModule.cs
@@ -0,0 +1,23 @@
+﻿using jobSalt.Models.Data_Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace jobSalt.Models.Feature.Housing
+{
+    interface IHousingModule
+    {
+        /// <summary>
+        /// The name to display to the user for this data source
+        /// </summary>
+        Source Source { get; }
+
+        /// <summary>
+        /// Asyncronously grab a set of Housing postings based off of the given filters and page number.
+        /// </summary>
+        /// <param name="filters">The filters that the module should use to query</param>
+        /// <returns>The Housing postings</returns>
+        HousingPost GetHousing(FilterBag filters);
+    }
+}
