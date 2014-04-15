82d2accac61eb0dd9b6241c772d841cb79a24ff2
David Lamont <del6597@rit.edu>
2014-04-14 23:42:44 -0400

    Add a housing module skeleton.

---
+++ jobSalt/jobSalt/Models/Feature/Housing/LocalModule/LocalHousingModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jobSalt.Models.Feature.Housing.LocalModule
+{
+    public class LocalHousingModule : IHousingModule
+    {
+        public Data_Types.Source Source
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Dictionary<string, List<HousingPost>> GetHousing(FilterBag filters)
+        {
+            
+            return null;
+        }
+    }
+}
