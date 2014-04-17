3a5b2a249779e7019086dcaeb7e9066f79f55584
Daniel Wallach <dywallach@dywallach.net>
2014-04-17 15:10:23 -0400

    forgot Module

---
+++ jobSalt/jobSalt/Models/Config/Module.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jobSalt.Models.Config
+{
+    public class Module
+    {
+        public string Name { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
